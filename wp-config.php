8cf1dfc507bd4bb03094e4f315f66705df60ca50
Markjonel02 <rellesmark43@gmail.com>
2024-08-05 10:33:44 +0800

    customize navigation

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,9 @@
 <?php
+// Begin AIOWPSEC Firewall
+if (file_exists('C:/laragon/www/wonderhomenaturals/aios-bootstrap.php')) {
+	include_once('C:/laragon/www/wonderhomenaturals/aios-bootstrap.php');
+}
+// End AIOWPSEC Firewall
 /**
  * The base configuration for WordPress
  *
---
+++ wp-content/mu-plugins/aios-firewall-loader.php
@@ -0,0 +1,6 @@
+<?php
+// Begin AIOWPSEC Firewall
+if (file_exists('C:/laragon/www/wonderhomenaturals/aios-bootstrap.php')) {
+	include_once('C:/laragon/www/wonderhomenaturals/aios-bootstrap.php');
+}
+// End AIOWPSEC Firewall
