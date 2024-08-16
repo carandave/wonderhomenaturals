b78258e3314b490a0170d7bb9a5ea23edbdadaa6
dave11 <davecarandang9@gmail.com>
2024-08-16 16:47:05 +0800

    Created Section for Displaying Producst in Homepage, Modified the Setup page in order to set the My account and Checkout to respective page, Installed Fibo Search Plugin, Installed SEARCHWP LIVE AJAX SEARCH to enhance live search functionality in products, Uninstalled Ajax search lite because there are conflicts between Fibo Search Plugin i can not search products, Created Search Functionality inside the header of FAQ's page, Export the Database

--- wp-content/themes/neve/header.php
+++ wp-content/themes/neve/header.php
@@ -40,6 +40,7 @@ do_action( 'neve_html_start_before' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
