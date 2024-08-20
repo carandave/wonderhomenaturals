2f9313dbad7793ccaad8680cf40daa1b07d99c68
dave11 <davecarandang9@gmail.com>
2024-08-20 16:36:21 +0800

    Modified the Themefile Editor to change the logo of login page, Installed Themify Plugin, Created attributes Scent and Size, Display the Side Bar with Color and SIZE fILTER

--- wp-content/themes/neve-child/functions.php
+++ wp-content/themes/neve-child/functions.php
@@ -5,3 +5,39 @@ add_action( 'wp_enqueue_scripts', 'wpchild_enqueue_styles' );
 function wpchild_enqueue_styles(){
   wp_enqueue_style( 'wpm-neve-style', get_template_directory_uri() . '/style.css' );
 }
+
+
+// Logo Change Start
+function add_logo_Login_page(){
+	wp_enqueue_style('custom-login', get_stylesheet_directory_uri() . '/style.css');
+
+}
+
+add_action('login_enqueue_scripts', 'add_logo_Login_page');
+// Logo Change End
+
+//Logo Set URL Start
+function login_page_url(){
+	return home_url('/wp-admin');
+}
+
+add_filter('login_headerurl', 'login_page_url');
+//Logo Set URL End
+
+
+  
+// function wtwh_delay_remove_result_count() {
+//    remove_action('woocommerce_after_shop_loop', 'woocommerce_result_count', 20);
+//    remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
+// }
+
+// add_action('wp', 'wtwh_delay_remove_result_count');
+
+function wtwh_remove_result_count() {
+    remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
+    remove_action('woocommerce_after_shop_loop', 'woocommerce_result_count', 20);
+}
+add_action('template_redirect', 'wtwh_remove_result_count');
+
+
+
